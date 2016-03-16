c92710e1d20a12b0edf0766c296ce0b6205c88ef
Anthony <cot.anthony@gmail.com>
2016-03-17 00:21:47 +0100

    a

--- vues/vueChampionnat.php
+++ vues/vueChampionnat.php
@@ -1,8 +1,11 @@
-<?php $this->titre = "Mon Blog - " . $championnats["pays_championnat"]; ?>
+<?php $this->titre = "Mon Blog - " ?>
 
-<?php foreach ($championnats as $championnat):
+
+<?php
+	foreach ($championnats as $championnat):
     ?>
     <article>
+    <h1>Championnats de <?php echo $championnat["pays_championnat"]?></h1>
         <header>
             <a href="<?= "index.php?action=classement&id=".$championnat["nom_championnat"] ?>">
             <?php echo $championnat["nom_championnat"]?></a>
